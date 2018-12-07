419731f3973c39e5341a926887e80528c4f05d88
Gerathain <mark@the-ellwoods.org>
2018-12-07 00:34:07 +0000

    added a number of new tests for readConfig and added some negative tests which fail

--- main_test.go
+++ main_test.go
@@ -21,3 +21,52 @@ func TestGetApiKey(t *testing.T) {
 		t.Fail()
 	}
 }
+func TestGetApiKeyMissingFile(t *testing.T) {
+	correctKey := "YaX2vXeBA3W-w6sNNwpkebTheJjAVPE0tgCQ86NG"
+
+	retVal := getApiKey( "fileDoesntExist" )
+	if correctKey != retVal {
+		t.Fail()
+	}
+}
+
+func TestReadConfig(t *testing.T) {
+	controllerOn := true
+	startHour := 21
+	startMin := 0
+	warmRate := 50
+	correctConfig := Config{ &controllerOn, &startHour, &startMin, &warmRate }
+
+	var retConfig Config
+	err := readConfig( "testConfig", &retConfig )
+	if err != nil {
+		t.Fail()
+		return
+	}
+	
+	// This is needed to cover all the values as the members are all pointers
+	if *(retConfig.ControllerOn) != *(correctConfig.ControllerOn) {
+		t.Fail()
+	}
+	if *(retConfig.StartHour) != *(correctConfig.StartHour) {
+		t.Fail()
+	}
+	if *(retConfig.StartMinute) != *(correctConfig.StartMinute) {
+		t.Fail()
+	}
+	if *(retConfig.WarmingRate) != *(correctConfig.WarmingRate) {
+		t.Fail()
+	}
+}
+func TestReadConfigMissingFile(t *testing.T) {
+	err := readConfig( "fileDoesntExist", nil )
+	if err != nil {
+		t.Fail()
+	}
+}
+func TestReadConfigNilConfig(t *testing.T) {
+	err := readConfig( "testConfig", nil )
+	if err != nil {
+		t.Fail()
+	}
+}
